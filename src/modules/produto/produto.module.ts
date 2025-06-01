b148c08d0470a5c9b27052e1c94c1f1756bc29d8
Filipi Romão <129067530+FilipiNyetz@users.noreply.github.com>
2025-06-01 11:17:55 -0300

    Update produto.module.ts

--- src/modules/produto/produto.module.ts
+++ src/modules/produto/produto.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProdutoService } from './produto.service';
 import { ProdutoController } from './produto.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Produto } from './entities/produto.entity';
+import { Loja } from 'src/loja/entities/loja.entity';
+
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Produto, Loja])],
   controllers: [ProdutoController],
-  providers: [ProdutoService],
+  providers: [ProdutoService], // adicione aqui
 })
-export class ProdutoModule {}
+export class ProdutoModule { }
