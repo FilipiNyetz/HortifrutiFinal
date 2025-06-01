ffaa694198ff94a46e23bed2fc74c003b312b583
Filipi Romão <129067530+FilipiNyetz@users.noreply.github.com>
2025-06-01 11:23:33 -0300

    create-carrinho.dto.ts

--- src/modules/carrinho/dto/create-carrinho.dto.ts
+++ src/modules/carrinho/dto/create-carrinho.dto.ts
@@ -1,1 +1,23 @@
-export class CreateCarrinhoDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateProdutoDto {
+    @IsString()
+    @IsNotEmpty()
+    nome: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    quantidade: number;
+ 
+    @IsString()
+    @IsNotEmpty()
+    id_Produto: string;
+ 
+    @IsString()
+    @IsNotEmpty()
+    id_Categoria: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    valor: number;
+}
