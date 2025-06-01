a69f0f240bd6e10105462235bdba4300d07043f7
Filipi Romão <129067530+FilipiNyetz@users.noreply.github.com>
2025-06-01 11:15:39 -0300

    produto.entity.ts

--- src/modules/produto/entities/produto.entity.ts
+++ src/modules/produto/entities/produto.entity.ts
@@ -1,1 +1,27 @@
-export class Produto {}
+import { BeforeInsert, PrimaryGeneratedColumn,Column, PrimaryColumn, Entity } from "typeorm";
+import { Loja } from 'src/loja/entities/loja.entity';
+
+@Entity('produto')
+export class Produto {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column()
+    nome: string; 
+
+    @Column()
+    quantidade: number;
+    
+    @Column()
+    id_Categoria: string;
+
+    @Column()
+    valor: number;
+
+  @ManyToOne(() => Loja, (loja) => loja.produtos, {
+        onDelete: 'CASCADE',
+        eager: true,
+    })
+    loja: Loja;
+    static loja: any;    
+}
