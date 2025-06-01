f856829d1b058257980bab0942e8892b3ac03448
Filipi Romão <129067530+FilipiNyetz@users.noreply.github.com>
2025-06-01 11:16:32 -0300

    create-produto.dto.ts

--- src/modules/produto/dto/create-produto.dto.ts
+++ src/modules/produto/dto/create-produto.dto.ts
@@ -1,1 +1,25 @@
-export class CreateProdutoDto {}
+import { IsString, IsNumber, IsPositive, IsInt } from 'class-validator';
+import { Loja } from 'src/loja/entities/loja.entity';
+import { ManyToOne } from 'typeorm';
+
+export class CreateProdutoDto {
+    @IsString()
+    nome: string;
+
+    @IsString()
+    descricao: string;
+
+    @IsNumber()
+    @IsPositive()
+    preco: number;
+
+    @IsInt()
+    @IsPositive()
+    quantidade: number;
+
+    @IsString()
+    categoria: string;
+
+    @IsNumber()
+    id_loja: number;
+}
