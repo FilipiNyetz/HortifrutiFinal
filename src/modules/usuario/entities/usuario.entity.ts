fedd166bf74910a82e0cea51a4d6735ff032dbc5
Gabriel Cézar <151453514+gbrielzera@users.noreply.github.com>
2025-06-09 19:55:23 -0300

    Atualização usuario relacionado com metodo pagamento

--- src/modules/usuario/entities/usuario.entity.ts
+++ src/modules/usuario/entities/usuario.entity.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { Endereco } from 'src/modules/endereco/entities/endereco.entity';
 import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { Compra } from 'src/modules/compra/entities/compra.entity';
 import { Favorito } from 'src/modules/favoritos/entities/favorito.entity';
 import { Avaliacao } from 'src/modules/avaliacao/entities/avaliacao.entity';
+import { MetodoPagamento } from 'src/modules/metodo-pagamento/entities/metodo-pagamento.entity';
 
 export enum UserRole {
   USER = 'USER',
@@ -44,4 +46,7 @@ export class Usuario {
 
   @OneToMany(() => Avaliacao, avaliacao => avaliacao.usuario)
   avaliacoes: Avaliacao[];
+
+  @OneToMany(() => MetodoPagamento, (metodo) => metodo.usuario)
+  metodosPagamento: MetodoPagamento[];
 }
